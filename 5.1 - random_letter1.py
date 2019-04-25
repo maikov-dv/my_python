6dbbbb450f7cf97fce3b963b8f879e34f60453c1
maikov-dv <49732587+maikov-dv@users.noreply.github.com>
2019-04-25 22:02:46 +0300

    Add files via upload

---
+++ 5.1 - random_letter1.py
@@ -0,0 +1,20 @@
+# угадывание случайной буквы из слова
+
+import random
+
+word_list = ["самовар", "весна", "лето"]
+
+word = random.choice(word_list)
+letter = random.choice(word)
+list_of_letters = list(word)
+index_of_letter = list_of_letters.index(letter)
+list_of_letters[index_of_letter] = "?"
+new_word = "".join(list_of_letters)
+print(new_word)
+
+user_letter = input("Введите букву: ")
+
+if user_letter == letter:
+    print("Победа!\n", "Слово:", word)
+else:
+    print("Увы! Попробуйте в другой раз.\n", "Слово:", word)
